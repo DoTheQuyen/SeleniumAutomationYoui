886eded0af34e6f9a50f3c7af3fbe2b900898a9c
DoTheQuyen <quyen.galiso@gmail.com>
2022-05-27 14:53:44 +1000

    Initial setup base reference

--- SeleniumAutomationYoui/UnitTestMain.cs
+++ SeleniumAutomationYoui/UnitTestMain.cs
@@ -9,13 +9,25 @@ namespace SeleniumAutomationYoui
 {
     public class Tests
     {
+        /// <summary>
+        /// Initial setup global variables for NUnit system when start testing
+        /// </summary>
         [SetUp]
         public void Setup()
         {
         }
 
+        /// <summary>
+        /// Finalise function when system finish all testcases
+        /// </summary>
+        [TearDown]
+        public void Finalise()
+        {
+
+        }
+
         [Test]
-        public void Test1()
+        public void Test()
         {
             Assert.Pass();
         }
